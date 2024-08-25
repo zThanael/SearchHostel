8aed4b15113049c099554557299e152cb20b8c19
Thanael <zthanaelbutewicz@hotmail.com>
2024-08-25 13:29:38 -0300

    Commit com nova visualização para testar

--- app.py
+++ app.py
@@ -33,11 +33,11 @@ st.markdown('''
 ''', unsafe_allow_html=True)
 
 # Remover espaçamento no TOPO
-st.markdown('''
-<style> 
-    div[class^='block-container'] { padding-top: 1rem; } 
-</style> 
-''', unsafe_allow_html=True)
+# st.markdown('''
+# <style> 
+#     div[class^='block-container'] { padding-top: 1rem; } 
+# </style> 
+# ''', unsafe_allow_html=True)
 
 # Get data in cache
 @st.cache_data
@@ -45,12 +45,32 @@ def get_data():
 
     # Get the credencial - Secrets
     string_connection = st.secrets['string_connection']
-
+   
     connection = create_engine(string_connection)
 
     # Create DataFrame
     df = pd.read_sql('''
-    select * from hostelworld_hostel hh  where "type" = 'HOSTEL' --and country = 'Brazil'
+	select
+            hostel."name" 
+        ,	hostel.qtd_rating 
+        ,	coalesce(ratings.rating::text, '') as rating
+        ,	case 
+                when ratings.rating >= 4 then 'green'
+                when ratings.rating >= 2.5 and ratings.rating < 4 then 'orange'
+                when ratings.rating < 2.5 then 'lightred'
+                else 'lightgray'
+            end as color
+        ,	hostel.continent 
+        ,	hostel.country 
+        ,	hostel.city 
+        ,	hostel.latitude 
+        ,	hostel.longitude 
+        ,	hostel.url 
+    from hostelworld_hostel hostel  
+    left join hostelworld_ratings ratings
+        on ratings.id_hostel = hostel.id_hostel 
+    where 1=1
+        and "type" = 'HOSTEL' 
     ''', connection)
     connection.dispose()
 
@@ -58,43 +78,6 @@ def get_data():
 
 # Get the DataFrame
 df = get_data()
-
-# Criação dos marcadores que serão usados nos Mapas
-
-# Get Colors used in Folium
-colors = ['green','lightgreen', 'blue', 'lightblue', 'cadetblue' , 'orange','purple', 'beige','darkred', 'darkblue', 'darkgreen', 'darkpurple', 'pink',  'gray', 'lightgray','red']
-
-list_color = []
-list_comprehension = [list_color.extend([color]*10) for color in colors]
-
-nums = []
-list_comprehension = [nums.extend(np.arange(0,10)) for i in range(len(colors))]
-
-df_color = pd.DataFrame({'color': list_color, 'num_icon':nums})
-df_color = df_color.reset_index()
-
-# Mapeamento para o BackGround-Color
-opacidade = 0.1
-color_mapping = {
-    'red': (255, 0, 0, opacidade),
-    'blue': (0, 0, 255, opacidade),
-    'green': (0, 128, 0, opacidade),
-    'purple': (128, 0, 128, opacidade),
-    'orange': (255, 165, 0, opacidade),
-    'darkred': (139, 0, 0, opacidade),
-    'beige': (245, 245, 220, opacidade),
-    'darkblue': (0, 0, 139, opacidade),
-    'darkgreen': (0, 100, 0, opacidade),
-    'cadetblue': (95, 158, 160, opacidade),
-    'darkpurple': (128, 0, 128, opacidade),
-    'white': (255, 255, 255, opacidade),
-    'pink': (255, 192, 203, opacidade),
-    'lightblue': (173, 216, 230, opacidade),
-    'lightgreen': (144, 238, 144, opacidade),
-    'gray': (128, 128, 128, opacidade),
-    'black': (0, 0, 0, opacidade),
-    'lightgray': (211, 211, 211, opacidade)
-}
 
 # Header da página 
 title, button_newsletter = st.columns([0.85, 0.15], gap = "large")
@@ -107,13 +90,12 @@ with button_newsletter:
     button_newsletter.html('<br>')
 
 # Secção do MAPA
-selects, maps = st.columns([0.25, 0.75], gap = "large")
+select_continent, select_country, select_city = st.columns([0.2, 0.2, 0.2], gap = "large")
 
-with selects:
-    st.html('<br><br>')
-
+with select_continent:
+    
     # Select Continent
-    continent = selects.selectbox(
+    continent = select_continent.selectbox(
         label = "Selecione um Continente: ",
         options = (df['continent'].sort_values().unique()),
         index = None
@@ -122,10 +104,9 @@ with selects:
     if continent != None:
         df = df[df['continent'] == continent]
 
-    st.html('<br>')
-
+with select_country: 
     # Select Country
-    country = selects.selectbox(
+    country = select_country.selectbox(
         label = "Selecione um Pais: ",
         options = (df['country'].sort_values().unique()),
         index = None
@@ -134,77 +115,89 @@ with selects:
     if country != None:
         df = df[df['country'] == country]
 
-    st.html('<br>')
+with select_city:
 
     # Select City
-    city = selects.selectbox(
+    city = select_city.selectbox(
         label = "Selecione uma Cidade: ",
         options = (df['city'].sort_values().unique()),
         index = None
     )
-    st.html('<br>')
 
     if city != None:
         df = df[df['city'] == city]
     
-with maps: 
+
     
-    #Token 
-    token = st.secrets['map_token']
-    px.set_mapbox_access_token(token)
+#Token 
+token = st.secrets['map_token']
+px.set_mapbox_access_token(token)
 
-    # Se não usar o filtro de Cidade usar PLOTLY
-    if city == None:
-        
-        # Criação do mapa com Plotly
-        fig = px.scatter_mapbox(df,
-                            lat='latitude',
-                            lon='longitude',
-                            hover_name="name",
-                            zoom = 12 if city else 2.2
-        )
+# Se não usar o filtro de Cidade usar PLOTLY
+if city == None:
+    
+    # Criação do mapa com Plotly
+    fig = px.scatter_mapbox(df,
+                        lat='latitude',
+                        lon='longitude',
+                        hover_name="name",
+                        zoom = 12 if city else 2.2
+    )
 
-        # Ajustar o tamanho.
-        fig.update_layout(height=500, width= 800)
+    # Ajustar o tamanho.
+    fig.update_layout(height=500, width= 800)
 
-        # Plotar
-        st.plotly_chart(fig, use_container_width=True)
+    # Plotar
+    st.plotly_chart(fig, use_container_width=True)
 
-    # Se usar o Filtro da Cidade usar o Folium
-    else:
+# Se usar o Filtro da Cidade usar o Folium
+else:
 
-        # Criar colunas de cores.
+    # Criar colunas de cores.
 
-        # Ordenação
-        df = df.sort_values(by = 'qtd_rating', ascending = False)
+    # Ordenação
+    df = df.sort_values(by = 'qtd_rating', ascending = False)
 
-        # Resetar o Index para usa-lo no Merge
-        df = df.reset_index()
+    # Resetar o Index para usa-lo no Merge
+    df = df.reset_index()
 
-        # Juntar ao DataFrame de marcadores (df_color)
-        df = df.merge(df_color[['color','num_icon']], how='left', left_index=True, right_index=True)
+    # Configuração inicial do Mapa - Localização
+    lat_avg = df['latitude'].mean()
+    lon_avg = df['longitude'].mean()
 
-        # Configuração inicial do Mapa - Localização
-        lat_avg = df['latitude'].mean()
-        lon_avg = df['longitude'].mean()
+    # Criação do Mapa
+    m = folium.Map(
+        location = [lat_avg, lon_avg],
+        zoom_start = 10
+    )
 
-        # Criação do Mapa
-        m = folium.Map(
-            location = [lat_avg, lon_avg],
-            zoom_start = 10
-        )
+    # Definição dos Marcadores
+    for _, row in df.iterrows():
+        # Criação do Marcador
+        #iframe = folium.IFrame(str(row['nome']) + '<br>' + 'Avaliações: 12 ' + '<br>' + f"HostelWorld: <a href='{row['link']}'> Link </a>"),
+        html = f''' <h3> {row['name']}   </h3>
+        <b> Avaliações: </b> {row['qtd_rating']}<br>
+        <b> Nota: </b> {row['rating']} ⭐  <br><br>
+        <b> HostelWorld: </b> <a href='{row['url']}' , target="_blank"> Link </a>  '''
 
-        # Definição dos Marcadores
-        for _, row in df.iterrows():
-            # Criação do Marcador
-            folium.Marker(
-                [row['latitude'], row['longitude']],                                                  # Localização
-                tooltip = row['name'],                                                                # Hover do Marcador
-                icon=folium.Icon(icon=f"{row['num_icon']}", prefix='fa', color=f"{row['color']}")     # Icone do Marcador
-            ).add_to(m) 
+        iframe = folium.IFrame(html,
+                            width=300,
+                            height = 135
+                            )
 
-        # Instanciar o mapa
-        events = st_folium(m,  width=800, height = 500)
+        popup = folium.Popup(iframe,
+                            max_width=300)
+
+
+        folium.Marker(
+            [row['latitude'], row['longitude']],                                                  # Localização
+            tooltip = row['name'],                                                                # Hover do Marcador
+            icon=folium.Icon(color=f"{row['color']}"),    # Icone do Marcador
+            popup = popup    
+        ).add_to(m) 
+
+    # Instanciar o mapa
+    events = st_folium(m,  width=1300, height = 600)
 
 # Estilização
 def negrito(v):
@@ -222,36 +215,3 @@ def apply_style(row):
             styles.append(f'background-color: {row["color"]};')
     return styles
 
-
-# Criação da Listagem
-if city != None :
-    st.title("Hostel List")
-
-    # Renomear a coluna e filtras quais colunas mostrar
-    df.rename(columns = {'num_icon':'Marcador'}, inplace = True)
-    df_view = df[['color','Marcador','name','qtd_rating','currency','city','country','checkin_start','checkin_end','url']].sort_values(by = 'qtd_rating', ascending = False)
-
-    df_view.rename(columns = {
-        'name': 'Hostel',
-        'city': 'Cidade',
-        'country': 'País',
-        'qtd_rating':'Avaliações',
-        'url': 'URL',
-        'currency': 'Moeda',
-        'checkin_start': 'Início Check-In',
-        'checkin_end': 'Fim Check-In',
-    }, inplace = True)
-
-    # Criar a Tabela no HTML
-    st.markdown(f'''
-    {(df_view.style
-        .map(negrito, subset=['Hostel','Cidade','URL','Marcador','Início Check-In','Fim Check-In'])
-        .hide()
-        .apply(apply_style, axis=1, subset=['color','Hostel','Marcador','Avaliações'])
-        .hide(subset=['color'], axis = 1)
-        .set_properties(**{'text-align': 'center'}, subset=pd.IndexSlice[:, ['Marcador','Início Check-In','Fim Check-In']])
-        .set_table_styles([{'selector': 'th', 'props': [('text-align', 'center')]}])
-        .format({'Início Check-In': lambda x : f'{x:.0f}:00',  'Fim Check-In': lambda x : f'{x:.0f}:00'})
-        .format(make_clickable, subset=['URL'])
-    ).to_html()}
-    ''', unsafe_allow_html=True)
---
+++ app_old.py
@@ -0,0 +1,258 @@
+# Importação das Bibliotecas
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+# Bibliotecas para os Mapas
+import plotly.express as px
+import plotly.graph_objects as go
+import folium
+from streamlit_folium import st_folium
+
+# Ajustes no CSS
+# Remover o Header
+hide_streamlit_style = """
+            <style>
+                /* Hide the Streamlit header and menu */
+                header {visibility: hidden;}
+                /* Optionally, hide the footer */
+                .streamlit-footer {display: none;}
+                /* Hide your specific div class, replace class name with the one you identified */
+                .st-emotion-cache-uf99v8 {display: none;}
+            </style>
+            """
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# Remover o Espaçamento Lateral
+st.markdown('''
+<style>
+    section.main > div {max-width:75rem}
+</style>
+''', unsafe_allow_html=True)
+
+# Remover espaçamento no TOPO
+st.markdown('''
+<style> 
+    div[class^='block-container'] { padding-top: 1rem; } 
+</style> 
+''', unsafe_allow_html=True)
+
+# Get data in cache
+@st.cache_data
+def get_data():
+
+    # Get the credencial - Secrets
+    #string_connection = st.secrets['string_connection']
+    string_connection = "postgresql+psycopg2://leitor:leitor123@personal.cxe0iuo8g20s.us-east-1.rds.amazonaws.com:5432/postgres"
+
+    connection = create_engine(string_connection)
+
+    # Create DataFrame
+    df = pd.read_sql('''
+    select * from hostelworld_hostel hh  where "type" = 'HOSTEL' --and country = 'Brazil'
+    ''', connection)
+    connection.dispose()
+
+    return df
+
+# Get the DataFrame
+df = get_data()
+
+# Criação dos marcadores que serão usados nos Mapas
+
+# Get Colors used in Folium
+colors = ['green','lightgreen', 'blue', 'lightblue', 'cadetblue' , 'orange','purple', 'beige','darkred', 'darkblue', 'darkgreen', 'darkpurple', 'pink',  'gray', 'lightgray','red']
+
+list_color = []
+list_comprehension = [list_color.extend([color]*10) for color in colors]
+
+nums = []
+list_comprehension = [nums.extend(np.arange(0,10)) for i in range(len(colors))]
+
+df_color = pd.DataFrame({'color': list_color, 'num_icon':nums})
+df_color = df_color.reset_index()
+
+# Mapeamento para o BackGround-Color
+opacidade = 0.1
+color_mapping = {
+    'red': (255, 0, 0, opacidade),
+    'blue': (0, 0, 255, opacidade),
+    'green': (0, 128, 0, opacidade),
+    'purple': (128, 0, 128, opacidade),
+    'orange': (255, 165, 0, opacidade),
+    'darkred': (139, 0, 0, opacidade),
+    'beige': (245, 245, 220, opacidade),
+    'darkblue': (0, 0, 139, opacidade),
+    'darkgreen': (0, 100, 0, opacidade),
+    'cadetblue': (95, 158, 160, opacidade),
+    'darkpurple': (128, 0, 128, opacidade),
+    'white': (255, 255, 255, opacidade),
+    'pink': (255, 192, 203, opacidade),
+    'lightblue': (173, 216, 230, opacidade),
+    'lightgreen': (144, 238, 144, opacidade),
+    'gray': (128, 128, 128, opacidade),
+    'black': (0, 0, 0, opacidade),
+    'lightgray': (211, 211, 211, opacidade)
+}
+
+# Header da página 
+title, button_newsletter = st.columns([0.85, 0.15], gap = "large")
+with title:
+    title.title('Search Hostel')
+
+with button_newsletter:
+    button_newsletter.html('<br>')
+    button_newsletter.link_button("Newsletter", "https://thanael.substack.com/", type='primary')
+    button_newsletter.html('<br>')
+
+# Secção do MAPA
+selects, maps = st.columns([0.25, 0.75], gap = "large")
+
+with selects:
+    st.html('<br><br>')
+
+    # Select Continent
+    continent = selects.selectbox(
+        label = "Selecione um Continente: ",
+        options = (df['continent'].sort_values().unique()),
+        index = None
+    )
+
+    if continent != None:
+        df = df[df['continent'] == continent]
+
+    st.html('<br>')
+
+    # Select Country
+    country = selects.selectbox(
+        label = "Selecione um Pais: ",
+        options = (df['country'].sort_values().unique()),
+        index = None
+    )
+
+    if country != None:
+        df = df[df['country'] == country]
+
+    st.html('<br>')
+
+    # Select City
+    city = selects.selectbox(
+        label = "Selecione uma Cidade: ",
+        options = (df['city'].sort_values().unique()),
+        index = None
+    )
+    st.html('<br>')
+
+    if city != None:
+        df = df[df['city'] == city]
+    
+with maps: 
+    
+    #Token 
+    #token = st.secrets['map_token']
+    token = "pk.eyJ1IjoidGhhbmFlbCIsImEiOiJjbHdmbnFsMWwwMjh1MmtvN3k4bDlibGZrIn0.GV0-K-dJuFBuCw7HxZnmYA"
+    px.set_mapbox_access_token(token)
+
+    # Se não usar o filtro de Cidade usar PLOTLY
+    if city == None:
+        
+        # Criação do mapa com Plotly
+        fig = px.scatter_mapbox(df,
+                            lat='latitude',
+                            lon='longitude',
+                            hover_name="name",
+                            zoom = 12 if city else 2.2
+        )
+
+        # Ajustar o tamanho.
+        fig.update_layout(height=500, width= 800)
+
+        # Plotar
+        st.plotly_chart(fig, use_container_width=True)
+
+    # Se usar o Filtro da Cidade usar o Folium
+    else:
+
+        # Criar colunas de cores.
+
+        # Ordenação
+        df = df.sort_values(by = 'qtd_rating', ascending = False)
+
+        # Resetar o Index para usa-lo no Merge
+        df = df.reset_index()
+
+        # Juntar ao DataFrame de marcadores (df_color)
+        df = df.merge(df_color[['color','num_icon']], how='left', left_index=True, right_index=True)
+
+        # Configuração inicial do Mapa - Localização
+        lat_avg = df['latitude'].mean()
+        lon_avg = df['longitude'].mean()
+
+        # Criação do Mapa
+        m = folium.Map(
+            location = [lat_avg, lon_avg],
+            zoom_start = 10
+        )
+
+        # Definição dos Marcadores
+        for _, row in df.iterrows():
+            # Criação do Marcador
+            folium.Marker(
+                [row['latitude'], row['longitude']],                                                  # Localização
+                tooltip = row['name']                                                                # Hover do Marcador
+            ).add_to(m) 
+
+        # Instanciar o mapa
+        events = st_folium(m,  width=800, height = 500)
+
+# Estilização
+def negrito(v):
+        return f"font-weight: bold; text_align: center"
+
+def make_clickable(val):
+    return '<a href="{}">Link</a>'.format(val)
+
+def apply_style(row):
+    styles = []
+    for val in row.index:
+        if val not in ('color','Marcador'): 
+            styles.append(f'background-color: rgba{color_mapping[row["color"]]};')
+        else:
+            styles.append(f'background-color: {row["color"]};')
+    return styles
+
+
+# Criação da Listagem
+if city != None :
+    st.title("Hostel List")
+
+    # Renomear a coluna e filtras quais colunas mostrar
+    df.rename(columns = {'num_icon':'Marcador'}, inplace = True)
+    df_view = df[['color','Marcador','name','qtd_rating','currency','city','country','checkin_start','checkin_end','url']].sort_values(by = 'qtd_rating', ascending = False)
+
+    df_view.rename(columns = {
+        'name': 'Hostel',
+        'city': 'Cidade',
+        'country': 'País',
+        'qtd_rating':'Avaliações',
+        'url': 'URL',
+        'currency': 'Moeda',
+        'checkin_start': 'Início Check-In',
+        'checkin_end': 'Fim Check-In',
+    }, inplace = True)
+
+    # Criar a Tabela no HTML
+    st.markdown(f'''
+    {(df_view.style
+        .map(negrito, subset=['Hostel','Cidade','URL','Marcador','Início Check-In','Fim Check-In'])
+        .hide()
+        .apply(apply_style, axis=1, subset=['color','Hostel','Marcador','Avaliações'])
+        .hide(subset=['color'], axis = 1)
+        .set_properties(**{'text-align': 'center'}, subset=pd.IndexSlice[:, ['Marcador','Início Check-In','Fim Check-In']])
+        .set_table_styles([{'selector': 'th', 'props': [('text-align', 'center')]}])
+        .format({'Início Check-In': lambda x : f'{x:.0f}:00',  'Fim Check-In': lambda x : f'{x:.0f}:00'})
+        .format(make_clickable, subset=['URL'])
+    ).to_html()}
+    ''', unsafe_allow_html=True)
